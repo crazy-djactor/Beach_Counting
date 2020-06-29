cf164197256ade360ca5b67cdfcf0fc6ad6550e8
Evgeny <evgenytikhonov@outlook.com>
2020-06-29 22:49:25 +0300

    edit init bash

--- init.sh
+++ init.sh
@@ -7,7 +7,7 @@ pip install opencv-contrib-python==4.2.0.*
 pip install lxml
 pip install bs4
 pip install tensorflow==1.13.1
-pip install requirements.txt
+pip install -r requirements.txt
 sudo apt update && apt install -y libsm6 libxext6 libxrender-dev
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1pIwotoRlgUH09eyQvKXnhquMSx8RDWQS' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1pIwotoRlgUH09eyQvKXnhquMSx8RDWQS" -O ./models/frozen_inference_graph_faster_50.pb && rm -rf /tmp/cookies.txt
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1AwqhHwOcxg7jq2iU_FeAMT-F4Z-qq3Va' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1AwqhHwOcxg7jq2iU_FeAMT-F4Z-qq3Va" -O ./models/frozen_inference_graph_faster_101.pb && rm -rf /tmp/cookies.txt
