1b7fc0a728c25192d366fc8873620ca732e77356
DJACTOR <babhan312@hotmail.com>
2020-07-10 13:44:07 -0400

    resize and processing

--- process_video.py
+++ process_video.py
@@ -129,7 +129,7 @@ class ProcessVideo:
                 if f_send_server:
                     temp_name = str(time.time()) + '.jpg'
 
-                    cv2.imwrite(temp_name, img_draw)
+                    # cv2.imwrite(temp_name, img_draw)
                     json_req = make_request_json(ip_addr=CAMERA_IP, img_file=temp_name, count=len(valid_rects),
                                                  cam_name=current_preset.Name)
                     send_request(server=SERVER_URL, cam_name=current_preset.Name, req_json=json_req)
